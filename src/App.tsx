8342fddf0487a74943e1112649aa73f6df3a2f37
KiberVG <martinezkimber03@gmail.com>
2024-04-20 17:03:59 -0400

    Revert "added real map"
    This reverts commit d228a4c23d61dcc06cdfa6ea4a278f057b58ae4c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 import Footer from './Footer'
-import Map from './Map'
-import Graph from './Graph'
 
 function App() {
-  const [disaster, setDisaster] = useState<"fire" | "flood" | "hurricane">("fire")
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Graph></Graph>
+        <div className="parent">
+          <div className="fire"></div>
+          <div className="flood"></div>
+          <div className="hurricane"></div>
+        </div>
 
+    <div className="fannie-mae-logo">
+      <img src="\src\assets\fannie-mae-logo-dark-blue.png"></img>
+    </div>
+
+    <div className="main-content">
+      < p>
+        <img src="\src\assets\US-Heatmap-PLACEHOLDER.png"></img>
+      </p>
+      <Footer></Footer>
+      <p>
+        Fannie Mae Logo® is a registered trademark of Fannie Mae.
+      </p>
+    </div>
     </>
   )
 }
