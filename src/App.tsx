bb35ca1f29a982cf9d9c60a1c92431824fcad731
Madeline Anson <madelineganson@gmail.com>
2024-04-20 15:19:41 -0400

    favicon

--- src/App.tsx
+++ src/App.tsx
@@ -15,39 +15,14 @@ function App() {
 
     <div className="main-content">
       < p>
-          <img src="\src\assets\US-Heatmap-PLACEHOLDER.png"></img>
-        </p>
-        <Footer></Footer>
-        <p>
+        <img src="\src\assets\US-Heatmap-PLACEHOLDER.png"></img>
+      </p>
+      <Footer></Footer>
+      <p>
         Fannie Mae Logo® is a registered trademark of Fannie Mae.
       </p>
     </div>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>hi</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-
-
-
-      
     </>
-
   )
 }
 
