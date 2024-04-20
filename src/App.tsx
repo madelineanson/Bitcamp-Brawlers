b6e4444bf99dd602febead1486e430d43b25da9a
jasonyih <jason@Jasons-MacBook-Air-6.local>
2024-04-20 16:56:52 -0400

    Made Graph component

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Footer from './Footer'
 import Map from './Map'
+import Graph from './Graph'
 
 function App() {
   const [disaster, setDisaster] = useState<"fire" | "flood" | "hurricane">("fire")
 
   return (
     <>
-        
+      <Graph></Graph>
 
-    <div className="fannie-mae-logo">
-      <img src="\src\assets\fannie-mae-logo-dark-blue.png"></img>
-    </div>
-
-    <div className="map-buttons">
-          <div onClick={() => setDisaster("fire")} className="fire"></div>
-          <div onClick={() => setDisaster("flood")} className="flood"></div>
-          <div onClick={() => setDisaster("hurricane")} className="hurricane"></div>
-    </div>
-
-    <div className="main-content">
-      <div className="maps">
-        <Map disaster={disaster}></Map>
-      </div>
-      <Footer></Footer>
-      <p>
-        Fannie Mae Logo® is a registered trademark of Fannie Mae.
-      </p>
-    </div>
     </>
   )
 }
