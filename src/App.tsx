d228a4c23d61dcc06cdfa6ea4a278f057b58ae4c
Madeline Anson <madelineganson@gmail.com>
2024-04-20 15:42:50 -0400

    added real map

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import { useState } from 'react'
 import './App.css'
 import Footer from './Footer'
+import Map from './Map'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [disaster, setDisaster] = useState<"fire" | "flood" | "hurricane">("fire")
 
   return (
     <>
-        <div className="parent">
-          <div className="fire"></div>
-          <div className="flood"></div>
-          <div className="hurricane"></div>
-        </div>
+        
 
     <div className="fannie-mae-logo">
       <img src="\src\assets\fannie-mae-logo-dark-blue.png"></img>
     </div>
 
+    <div className="map-buttons">
+          <div onClick={() => setDisaster("fire")} className="fire"></div>
+          <div onClick={() => setDisaster("flood")} className="flood"></div>
+          <div onClick={() => setDisaster("hurricane")} className="hurricane"></div>
+    </div>
+
     <div className="main-content">
-      < p>
-        <img src="\src\assets\US-Heatmap-PLACEHOLDER.png"></img>
-      </p>
+      <div className="maps">
+        <Map disaster={disaster}></Map>
+      </div>
       <Footer></Footer>
       <p>
         Fannie Mae Logo® is a registered trademark of Fannie Mae.
